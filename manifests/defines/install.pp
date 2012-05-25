038beccb88215e6ca4c712404611f6395c071398
Brandon Helwig <brhelwig@brhelwig.com>
2012-05-25 02:27:25 -0600

    rvm is a function...this is going to be fun

--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -1,6 +1,6 @@
 define rvm::install($ruby = $name, $user) {
 exec { rvm_ruby_install : 
-    command => "/home/${user}/.rvm/bin/rvm install ${ruby}",
+    command => "/bin/bash -l -c /home/${user}/.rvm/bin/rvm install ${ruby}",
     user => $user,
     creates => "/home/${user}/.rvm/bin/${ruby}",
 }
