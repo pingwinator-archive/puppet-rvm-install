496de223fa2db345f2c76530e30562a4a9fcca07
Brandon Helwig <brhelwig@brhelwig.com>
2012-05-29 14:37:45 -0600

    debugging

--- manifests/defines/gemset.pp
+++ manifests/defines/gemset.pp
@@ -1,6 +1,6 @@
 define rvm::gemset($gemset = $name, $ruby, $user) {
 exec { rvm_gemset_create : 
-    command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/bin/rvm-shell gemset create ${gemset}",
+    command => "/usr/bin/sudo -i -u ${user} rvm use ${ruby}@${gemset} --create",
     creates => "/home/${user}/.rvm/bin/${ruby}@${gemset}",
 }
 
--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -1,7 +1,7 @@
 define rvm::install($ruby = $name, $user) {
 exec { rvm_ruby_install : 
-    command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/bin/rvm-shell install ${ruby}",
-    creates => "/home/${user}/.rvm/bin/${ruby}",
+    command => "/usr/bin/sudo -i -u ${user} rvm use ${ruby} --install",
+    creates => "/home/${user}/.rvm/bin/${ruby}",∂
 }
 
 }
