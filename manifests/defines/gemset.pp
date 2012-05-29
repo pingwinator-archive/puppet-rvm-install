9d65aa853c7ae6ba59303e225c86bcb4dbda3947
Brandon Helwig <brhelwig@brhelwig.com>
2012-05-29 17:28:20 -0600

    fixes

--- manifests/defines/gemset.pp
+++ manifests/defines/gemset.pp
@@ -1,5 +1,5 @@
 define rvm::gemset($gemset = $name, $ruby, $user) {
-exec { rvm_gemset_create : 
+exec { "rvm_gemset_create-${ruby}-${user}-${gemset}" : 
     command => "/usr/bin/sudo -i -u ${user} rvm use ${ruby}@${gemset} --create",
     creates => "/home/${user}/.rvm/bin/${ruby}@${gemset}",
 }
