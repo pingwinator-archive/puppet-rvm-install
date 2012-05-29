71a1e6e6c466631f4d1464edd943a6646e2f6a30
Brandon Helwig <brhelwig@brhelwig.com>
2012-05-29 14:40:24 -0600

    debugging

--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -1,7 +1,7 @@
 define rvm::install($ruby = $name, $user) {
 exec { rvm_ruby_install : 
     command => "/usr/bin/sudo -i -u ${user} rvm use ${ruby} --install",
-    creates => "/home/${user}/.rvm/bin/${ruby}",∂
+    creates => "/home/${user}/.rvm/bin/${ruby}",
 }
 
 }
