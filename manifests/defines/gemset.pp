d34367a8a0a7d104631b49f5d3fc95713656e94f
Brandon Helwig <brhelwig@brhelwig.com>
2012-06-04 14:41:36 -0600

    fixes

--- manifests/defines/gemset.pp
+++ manifests/defines/gemset.pp
@@ -1,13 +1,13 @@
 define rvm::gemset($gemset = $name, $ruby, $user) {
 
-    file { "rvm_ruby_gemset_wrapper-${user}" :
-        path => "/home/${user}/.rvm/rvm_ruby_gemset_wrapper",
-        mode => 500,
-        replace => true,
-        content => "#!/usr/bin/env bash
-    source /home/${user}/.rvm/bin/rvm
-    rvm use $1@$2 --create"
-    }
+file { "rvm_ruby_gemset_wrapper-${user}" :
+    path => "/home/${user}/.rvm/rvm_ruby_gemset_wrapper",
+    mode => 500,
+    replace => true,
+    content => "#!/usr/bin/env bash
+source /home/${user}/.rvm/bin/rvm
+rvm use $1@$2 --create"
+}
 
 exec { "rvm_gemset_create-${ruby}-${user}-${gemset}" : 
     command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/rvm_ruby_gemset_wrapper ${ruby} ${gemset}",
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,4 @@
 import "defines/*.pp"
 
 class rvm {
-	
-	file { rvm_ruby_install_wrapper:
-	    path => "/root/.kerlrc",
-	    replace => true,
-	    content => "KERL_DOWNLOAD_DIR=${srcpath}/source
-	KERL_BUILD_DIR=${srcpath}/builds
-	KERL_DEFAULT_INSTALL_DIR=${binpath}"
-	}
-    
 }
