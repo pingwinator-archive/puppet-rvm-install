e858c4de21365f8c77ccb52b6e88987c53b78113
Brandon Helwig <brhelwig@brhelwig.com>
2012-06-04 14:30:29 -0600

    fixes

--- manifests/defines/gemset.pp
+++ manifests/defines/gemset.pp
@@ -9,13 +9,8 @@ define rvm::gemset($gemset = $name, $ruby, $user) {
     rvm use $1@$2 --create"
     }
 
-    exec { "rvm_ruby_install-${ruby}-${user}" : 
-        command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/rvm_ruby_install_wrapper ${ruby}",
-        creates => "/home/${user}/.rvm/bin/${ruby}",
-    }
-
 exec { "rvm_gemset_create-${ruby}-${user}-${gemset}" : 
-    command => "/usr/bin/sudo -i -u ${user} rvm use ${ruby}@${gemset} --create",
+    command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/rvm_ruby_gemset_wrapper ${ruby} ${gemset}",
     creates => "/home/${user}/.rvm/bin/${ruby}@${gemset}",
 }
 
--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -12,6 +12,7 @@ rvm install $1"
 exec { "rvm_ruby_install-${ruby}-${user}" : 
     command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/rvm_ruby_install_wrapper ${ruby}",
     creates => "/home/${user}/.rvm/bin/${ruby}",
+    require => File["rvm_ruby_install_wrapper-${user}"]
 }
 
 }
