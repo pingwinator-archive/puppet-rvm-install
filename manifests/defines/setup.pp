90c4c46508394589c063414443bc70d9a69e6903
Brandon Helwig <brhelwig@brhelwig.com>
2012-06-05 15:39:40 -0600

    working on rvm autobuild

--- manifests/defines/setup.pp
+++ manifests/defines/setup.pp
@@ -16,12 +16,12 @@ define rvm::setup(
     }
 
     exec { rvm_install:
-        command => "/usr/bin/sudo -i -u ${user} ${src_dir}/rvm-installer --version ${version} > /home/${user}/.rvm_install.log 2>&1",
+        command => "${src_dir}/rvm-installer --version ${version} > /home/${user}/.rvm_install.log 2>&1",
         require => Exec["rvm_download"],
         creates => "/home/${user}/.rvm",
         provider => "shell",
-        cwd => "/tmp/"
-        #user => $user
+        cwd => "/tmp/",
+        user => $user
     }
 
 }
