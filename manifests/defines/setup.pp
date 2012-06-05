31aae109be7abed351f072596f536800a31f1352
Brandon Helwig <brhelwig@brhelwig.com>
2012-06-05 15:12:24 -0600

    working on rvm autobuild

--- manifests/defines/setup.pp
+++ manifests/defines/setup.pp
@@ -4,15 +4,9 @@ define rvm::setup(
 ) {
 
 exec { rvm_download :
-    command =>"curl -L get.rvm.io | bash -s #{version}",
+    command =>"curl -L get.rvm.io | bash -s ${version}",
     user => $user,
-    creates => "${src_dir}/rvm-installer",
-    notify => File["rvm-installer"]
-}
-
-file { 'rvm-installer' : 
-    path => "${src_dir}/rvm-installer",
-    mode => 755,
+    unless => "grep ${version} /home/${user}/.rvm/VERSION",
 }
 
 
