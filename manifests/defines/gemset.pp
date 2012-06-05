786b434d9f1a2baaee60012f97395b60b1c2779c
Brandon Helwig <brhelwig@brhelwig.com>
2012-06-05 16:39:44 -0600

    slowly stepping thru

--- manifests/defines/gemset.pp
+++ manifests/defines/gemset.pp
@@ -3,7 +3,7 @@ define rvm::gemset($gemset = $name, $ruby, $user) {
 exec { "rvm_gemset_create-${ruby}-${user}-${gemset}" : 
     command => "/usr/bin/sudo -i -u ${user} /home/${user}/.rvm/bin/rvm_gemset_install ${ruby} ${gemset}",
     creates => "/home/${user}/.rvm/bin/${ruby}@${gemset}",
-    require => File["rvm_ruby_gemset_wrapper-${user}"]
+    require => [File["rvm_ruby_gemset_wrapper-${user}"], Exec["rvm_ruby_install-${ruby}-${user}"]]
 }
 
 }
