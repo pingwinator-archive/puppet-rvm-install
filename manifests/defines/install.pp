cb259e16e9c08880b75d0b978f17e5db32b0ae9a
Brandon Helwig <brhelwig@brhelwig.com>
2012-09-08 20:11:05 -0700

    don't install if already installed

--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -3,7 +3,7 @@ define rvm::install($user) {
 exec { "install-${name}-${user}" :
         command => "/bin/bash -l -c 'rvm use ${name} --create --install' > /home/${user}/.rvm/${name}_install.log 2>&1",
         require => Exec["rvm_install_${user}"],
-        #creates => "/home/${user}/.rvm",
+        creates => "/home/${user}/.rvm/bin/${name}",
         provider => "shell",
         user => $user,
         cwd => "/tmp/"
