1e69081b857a341113f1be06727d832e870b4252
Brandon Helwig <brhelwig@brhelwig.com>
2012-09-08 20:27:25 -0700

    seems it probably needs to be a login shell

--- manifests/defines/setup.pp
+++ manifests/defines/setup.pp
@@ -5,7 +5,7 @@ define rvm::setup(
 ) {
 
     exec { "rvm_install_${user}" :
-        command => "/bin/bash -c '${src_dir}/rvm-installer --version ${version}' > /home/${user}/.rvm_install.log 2>&1",
+        command => "/bin/bash -l -c '${src_dir}/rvm-installer --version ${version}' > /home/${user}/.rvm_install.log 2>&1",
         require => Exec["rvm_download"],
         creates => "/home/${user}/.rvm",
         provider => "shell",
