8ef73bb47b8162c2a99093c15f8cdad428215327
Brandon Helwig <brhelwig@brhelwig.com>
2012-11-06 15:16:09 -0800

    this should be correct

--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -1,6 +1,7 @@
 define rvm::install($user) {
 
     require rvm
+    require Rvm:Setup[$user]
 
     exec { "rvm_install_${name}_for_${user}" :
         command => "sudo -i -u ${user} bash -l -c 'rvm use ${name} --create --install > /home/${user}/.rvm/${name}_install.log'",
