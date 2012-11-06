26e93909e674ba4fc6a14e52c1bcc7688739b08f
Brandon Helwig <brhelwig@brhelwig.com>
2012-11-06 14:41:17 -0800

    working on improving rvm deployment

--- manifests/defines/install.pp
+++ manifests/defines/install.pp
@@ -1,13 +1,12 @@
 define rvm::install($user) {
 
     require rvm
-    require rvm::setup($user)
 
     exec { "rvm_install_${name}_for_${user}" :
         command => "sudo -i -u ${user} bash -l -c 'rvm use ${name} --create --install > /home/${user}/.rvm/${name}_install.log'",
-        require => Exec["rvm_setup_${user}"],
         creates => "/home/${user}/.rvm/bin/${name}",
-        cwd => "/tmp/"
+        cwd => "/tmp/",
+        require => Rvm::Setup[$user],
     }
 
 }
